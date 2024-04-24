9f849c23341c62cf0fb0e5581e586cd3020734a5
chris-langfield <christopher.langfield@internationalbrainlab.org>
2024-04-23 21:05:49 -0400

    stack objects and rect_fft

--- src/hexfft/hexfft.py
+++ src/hexfft/hexfft.py
@@ -9,9 +9,102 @@ import numpy as np
 from hexfft.utils import mersereau_region, hex_to_pgram, pgram_to_hex
 from hexfft.array import HexArray, rect_shift, rect_unshift
 
+import logging
+_logger = logging.getLogger("hexfft")
+
+
+def FFT(shape, periodicity="rect", dtype=np.float32):
+    if periodicity == "hex":
+        raise NotImplementedError("Stack FFT not implemented yet for hex periodicity")
+    elif periodicity == "rect":
+        return RectPeriodicFFT(shape, dtype)
+
+
+class HexagonalFFT:
+    def __init__(self, shape, dtype):
+        self.shape = shape
+        self.dtype = dtype
+        self.precompute()
+
+    def precompute(self):
+        self._precompute()
+
+    def forward(self, x):
+        return self._forward(x)
+
+    def inverse(self, X):
+        return self._inverse(X)
+
+
+class HexPeriodicFFT(HexagonalFFT):
+    def __init__(self, shape, dtype):
+        super().__init__(self, shape, dtype)
+
+    def _precompute(self):
+        pass
+
+    def _forward(self, x):
+        pass
+
+    def _inverse(self, X):
+        pass
+
+
+class RectPeriodicFFT(HexagonalFFT):
+    def __init__(self, shape, dtype):
+        super().__init__(shape, dtype)
+
+    def _precompute(self):
+        N1, N2 = self.shape
+        self.phase_shift = np.exp(
+            1.0j * np.pi * np.array([i * np.arange(N2) for i in range(N1)]) / N1
+        )
+        self.phase_shift_conj = np.conj(self.phase_shift)
+
+    def _forward(self, x):
+        assert x.shape[-2:] == self.shape
+        if isinstance(x, HexArray) and x.pattern == "oblique":
+            _logger.warn("Passing HexArray with 'oblique' sampling pattern to "
+                         "a rectangular periodic FFT.")
+        else:
+            x = HexArray(x, "offset")
+        
+        x = rect_shift(x)
+
+        squeeze = x.ndim == 2
+        if squeeze:
+            x = np.expand_dims(x, 0)
+
+        F1 = np.fft.fft(x, axis=1)
+        F2 = F1 * self.phase_shift
+        X = np.fft.fft(F2, axis=2)
+
+        if squeeze:
+            X = np.squeeze(X)
+
+        return rect_unshift(HexArray(X, "oblique"))
+
+    def _inverse(self, X):
+
+        if isinstance(X, HexArray) and X.pattern == "oblique":
+            _logger.warn("Passing HexArray with 'oblique' sampling pattern to "
+                         "a rectangular periodic FFT.")
+        
+        X = rect_shift(X)
+        squeeze =  X.ndim == 2
+        if squeeze:
+            X = np.expand_dims(X, 0)
+        F2 = np.fft.ifft(X, axis=2)
+        F1 = F2 * self.phase_shift_conj
+        x = HexArray(np.fft.ifft(F1, axis=1))
+
+        if squeeze:
+            x = np.squeeze(x)
+
+        return rect_unshift(x)
+        
 
 def fft(x, hexcrop=False, periodicity="rect", dtype=np.float32):
-
     if periodicity == "rect":
         assert (
             hexcrop == False
@@ -26,8 +119,8 @@ def fft(x, hexcrop=False, periodicity="rect", dtype=np.float32):
         return pgram_to_hex(PX, N, pattern=x.pattern)
 
     # rectangular region
-    #assert x.shape[1] % 2 == 0
-    X = _rect_dft_slow(rect_shift(x))
+    # assert x.shape[1] % 2 == 0
+    X = rect_fft(rect_shift(x))
     return rect_unshift(X)
 
 
@@ -46,8 +139,8 @@ def ifft(X, hexcrop=False, periodicity="rect", dtype=np.complex64):
         return pgram_to_hex(px, N, pattern=X.pattern)
 
     # rectangular region
-    #assert X.shape[1] % 2 == 0
-    x = _rect_idft_slow(rect_shift(X))
+    # assert X.shape[1] % 2 == 0
+    x = rect_ifft(rect_shift(X))
     return rect_unshift(x)
 
 
@@ -190,7 +283,7 @@ def mersereau_ifft(PX):
 def fftshift(X):
     N = X.shape[0]
     n1, n2 = X.indices
-    m = mersereau_region(X).astype(bool)
+    m = mersereau_region(N, X.pattern).astype(bool)
     shifted = HexArray(np.zeros_like(X), X.pattern)
     if X.pattern == "oblique":
         regI = (n1 < N // 2) & (n2 < N // 2)
@@ -226,7 +319,7 @@ def fftshift(X):
 def ifftshift(X):
     N = X.shape[0]
     n1, n2 = X.indices
-    m = mersereau_region(X).astype(bool)
+    m = mersereau_region(N, X.pattern).astype(bool)
     shifted = HexArray(np.zeros_like(X), X.pattern)
 
     if X.pattern == "oblique":
@@ -295,7 +388,7 @@ def _hexdft_slow(x):
     n1, n2 = np.meshgrid(np.arange(N), np.arange(N))
     kern = _hexagonal_kernel(n1, n2, cdtype)
 
-    support = mersereau_region(x)
+    support = mersereau_region(N)
 
     X = HexArray(np.zeros(x.shape, cdtype))
     for w1 in range(N):
@@ -315,7 +408,7 @@ def _hexidft_slow(X):
 
     kern = np.conj(_hexagonal_kernel(n1, n2, cdtype))
 
-    support = mersereau_region(X)
+    support = mersereau_region(N)
 
     x = HexArray(np.zeros(X.shape, cdtype))
     for x1 in range(N):
@@ -411,6 +504,30 @@ def rect_kernel(n1, n2, cdtype):
                 * ((1 / (N1)) * w1 * (n1 - n2 / 2) + (1 / (N2)) * (n2) * (w2))
             )
     return kernel
+
+
+def rect_fft(x):
+    N1, N2 = x.shape
+    F1 = HexArray(np.fft.fft(x, axis=0))
+    exp_factor = np.exp(
+        1.0j * np.pi * np.array([i * np.arange(N2) for i in range(N1)]) / N1
+    )
+    F2 = F1 * exp_factor
+    F = HexArray(np.fft.fft(F2, axis=1))
+
+    return F
+
+
+def rect_ifft(X):
+    N1, N2 = X.shape
+    F2 = np.fft.ifft(X, axis=1)
+    exp_factor = np.exp(
+        -1.0j * np.pi * np.array([i * np.arange(N2) for i in range(N1)]) / N1
+    )
+    F1 = F2 * exp_factor
+    x = HexArray(np.fft.ifft(F1, axis=0))
+
+    return x
 
 
 def _rect_dft_slow(x):
